eabb6f162124359f34fc3e47cd441ed805096ecf
got12345678 <gotg16520@gmail.com>
2024-09-14 11:32:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bbnnQ/Emma2",
+    "https://github.com/got12345678/fhffjfghcgbnv",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
